4fee4aa7edbb3f76429987a96ec39a6056fe4827
Easonyesheng <preacher@sjtu.edu.cn>
2025-12-20 13:52:14 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,9 @@
+'''
+Author: Easonyesheng preacher@sjtu.edu.cn
+Date: 2025-12-20 13:47:34
+LastEditors: Easonyesheng preacher@sjtu.edu.cn
+LastEditTime: 2025-12-20 13:47:56
+FilePath: /research/ref_ray/third_party/Depth-Anything-3/__init__.py
+Description: import Depth-Anything-3 package
+'''
+from .src.depth_anything_3 import api
